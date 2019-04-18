1fc943388bb0ed9e7bde71a73bbac65436af8779
nsgindt <nsgindt@gmail.com>
2019-04-18 14:54:21 -0500

    Add files via upload

--- porch_lights/static/js/main.js
+++ porch_lights/static/js/main.js
@@ -41,28 +41,51 @@ $(document).ready(function() {
     socket.on('process_status', function(msg) {
         $('#process_status').html( 'step ' + msg.step + ' of ' + msg.total_steps);
     });
-
-    $( "#stopme" ).click(function() {
-      socket.emit('stopme');
-    });
-    $( "#startme" ).click(function() {
-      socket.emit('startme');
-    });
 */
    $( ".color-picker" ).click(function() {
-      $('#color-option').val(this.id);
-      console.log("red clicked");
+        $('#color-option').val(this.id);
+        $('#color-option').trigger("change");
     });
 
    $( ".pattern-picker" ).click(function() {
       $('#pattern-option').val(this.id);
-      console.log("red clicked");
+      $('#pattern-option').trigger("change");
+    });
+
+   $( ".color-one-picker" ).click(function() {
+      $('#color-one-option').val(this.id);
+      $('#color-one-option').trigger("change");
+    });
+
+   $( ".color-two-picker" ).click(function() {
+      $('#color-two-option').val(this.id);
+      $('#color-two-option').trigger("change");
+    });
+
+    $( ".color-three-picker" ).click(function() {
+      $('#color-three-option').val(this.id);
+      $('#color-three-option').trigger("change");
     });
 
     $( "#startme" ).click(function() {
-        $color = $('#color-option').val();
-        $pattern = $('#pattern-option').val();
-        $url = "/start/"+$color + "/" + $pattern
+      $color = $('#color-option').val();
+      $pattern = $('#pattern-option').val();
+      $color1 = $('#color-one-option').val();
+      $color2 = $('#color-two-option').val();
+      $color3 = $('#color-three-option').val();
+
+      
+      if ($color == "Rainbow" ){
+        $url = "/start/"+$color + "/" + $pattern;
+      } else if ($color== "Solid-Fade"){
+        $url = "/start/"+$color + "/" + $pattern;
+      } else if ($color== "One-Color"){
+        $url = "/start/"+$color + "/" + $color1 + "/" + $pattern;
+      } else if ($color=="Two-Color"){
+        $url = "/start/"+$color + "/" + $color1 + "/" + $color2 + "/" + $pattern;        
+      } else if ($color=="Three-Color"){
+        $url = "/start/"+$color + "/" + $color1 + "/" + $color2 + "/" + $color3 + "/" + $pattern;          
+      };
       window.location.href = $url
     });
 
@@ -71,7 +94,76 @@ $(document).ready(function() {
       window.location.href = $url
     });
 
+    $( "#color-option" ).change(function() {
+        $color = $("#color-option").val();
+        if ($color == "Rainbow" ){
+          hide_color_one();
+          hide_color_two();
+          hide_color_three();
+        } else if ($color== "Solid-Fade"){
+          hide_color_one();
+          hide_color_two();
+          hide_color_three();
+        } else if ($color== "One-Color"){
+          show_color_one();
+          hide_color_two();
+          hide_color_three();
+        } else if ($color=="Two-Color"){
+          show_color_one();
+          show_color_two();
+          hide_color_three();          
+        } else if ($color=="Three-Color"){
+          show_color_one();
+          show_color_two();
+          show_color_three();          
+        };
+    });
+
 });            
+/*        if ($(".progress-bar").hasClass("bg-success")){
+            $( ".progress-bar" ).removeClass( "bg-success" ).addClass( "bg-info" );
+        }*/
+
+function hide_color_one(){
+  if ($('#color-one').hasClass("d-none")){
+  } else {
+    $('#color-one').addClass( "d-none" );
+  };
+};
+
+function show_color_one(){
+  if ($('#color-one').hasClass("d-none")){
+     $('#color-one').removeClass( "d-none" ); 
+  };  
+};
+
+function hide_color_two(){
+  if ($('#color-two').hasClass("d-none")){
+  } else {
+    $('#color-two').addClass( "d-none" );
+  };
+};
+
+function show_color_two(){
+  if ($('#color-two').hasClass("d-none")){
+     $('#color-two').removeClass( "d-none" ); 
+  };  
+};
+function hide_color_three(){
+  if ($('#color-three').hasClass("d-none")){
+  } else {
+    $('#color-three').addClass( "d-none" );
+  };
+};
+
+function show_color_three(){
+  if ($('#color-three').hasClass("d-none")){
+     $('#color-three').removeClass( "d-none" ); 
+  };  
+};
+
+
+
 
 /*$(document).on( "click", ".color-picker", function(ev) {
 
